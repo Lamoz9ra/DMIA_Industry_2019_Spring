dd8208850204e205ae5d3abc0d3a583c2449a4a4
Lamoz9ra <43520922+Lamoz9ra@users.noreply.github.com>
2019-04-13 17:33:17 +0300

    Revert "SEM8"

--- seminar08/ml_numba/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='ml_numba',
-    install_requires=['numpy', 'numba'],
-    packages=find_packages(include=('ml_numba*',))
-)
--- seminar08/ml_pure/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='ml_pure',
-    install_requires=['numpy'],
-    packages=find_packages(include=('ml_pure*',))
-)
